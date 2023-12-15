62cc821de3f9e1dad58d208a484c3972f48ff61c
dcolucciInception <colucci.damiano.98@gmail.com>
2023-12-15 19:59:48 +0100

    pre-push

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# if [ ! -d /var/www/wordpress/admin ]; then
+if ! wp --path=/var/www/wordpress user list --field=user_login | /bin/grep -q "${DB_USERNAME}"; then
 
-# wp-administrator user creation
-wp core install --path=/var/www/wordpress --title=Inception_dcolucci --admin_user=dcolucci --admin_password=pippo --admin_email=sample@mail.it --url=www.dcolucci.42.fr
+wp core install --path=/var/www/wordpress --title=Inception_dcolucci --admin_user="${WP_ADMIN}" --admin_password="${WP_PASS}" --admin_email=sample@mail.it --url=www.dcolucci.42.fr
 
-# wp-user creation
 wp user create "${WP_USERNAME}" sample2@mail.it --path=/var/www/wordpress --role=author --user_pass="${WP_USER_PASS}"
 
-# fi
+wp plugin install wp-redis --activate --path=/var/www/wordpress
+
+fi
 
 php-fpm81 --nodaemonize
